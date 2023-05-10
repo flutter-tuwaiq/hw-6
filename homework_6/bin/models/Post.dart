edf43c10f0fbd699616a5587ac9f954574f3dce7
AhmadAlmudhi <a.almudhi1419@gmail.com>
2023-05-10 18:02:58 +0300

    Solve: homework 6

---
+++ homework_6/bin/models/Post.dart
@@ -0,0 +1,39 @@
+class Post {
+  int? id;
+  String? title;
+  String? body;
+  int? userId;
+  List? tags;
+  int? reactions;
+
+  Post({
+    this.id,
+    this.title,
+    this.body,
+    this.userId,
+    this.tags,
+    this.reactions,
+  });
+
+  factory Post.fromJson({required Map json}) {
+    return Post(
+      id: json['id'],
+      title: json['title'],
+      body: json['body'],
+      userId: json['userId'],
+      tags: json['tags'],
+      reactions: json['reactions'],
+    );
+  }
+
+  toMap() {
+    return {
+      "id": id,
+      "title": title,
+      "body": body,
+      "userId": userId,
+      "tags": tags,
+      "reactions": reactions,
+    };
+  }
+}
